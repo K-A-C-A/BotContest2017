313634f98111d8831ed1dbd6c9122e0423ec45aa
Louveau Alexandre <alexandre.louveau31@gmail.com>
2017-04-07 11:44:31 +0200

    Update ResponsiveBotTest.java

--- botContest01/src/main/java/com/mycompany/botcontest01/ResponsiveBotTest.java
+++ botContest01/src/main/java/com/mycompany/botcontest01/ResponsiveBotTest.java
@@ -284,7 +284,7 @@ public class ResponsiveBotTest extends UT2004BotModuleController<UT2004Bot> {
             navigation.stopNavigation();
             item = null;
         }
-        getAct().act(new Rotate().setAmount(3600));
+        getAct().act(new Rotate().setAmount(25555));
     }
 
     //////////////////////
